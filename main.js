b949a6456d1244a2034606ba2f246364b2b8a65a
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-11 21:41:07 +0200

    added send function via enter key

--- main.js
+++ main.js
@@ -1,15 +1,27 @@
 /*DNT*/$(document).ready(function() {/*DNT*/
 
   $('.fa-microphone').click(function() {
-
-    var newMsgText = $('#type-input').val();
-    console.log(newMsgText);
-
-    var newMsg = $('.template .msg').clone();
-
-    newMsg.text(newMsgText);
-
-    $('.msgs-container').append(newMsg);
+    sendMessage();
   })
 
+  $(document).keypress(function(event){
+    var keycode = (event.keyCode ? event.keyCode : event.which);
+    if(keycode == '13'){
+    sendMessage();
+    }
+  });
+
 })/*DNT*/
+
+/*FUNCTIONS*/
+
+function sendMessage() {
+  var newMsgText = $('#type-input').val();
+  console.log(newMsgText);
+
+  var newMsg = $('.template .msg').clone();
+
+  newMsg.text(newMsgText);
+
+  $('.msgs-container').append(newMsg);
+};
