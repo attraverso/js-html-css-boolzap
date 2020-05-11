bfd79fbe7e382391c78d0a9ac9370a9eff1e35dd
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-11 21:24:55 +0200

    added send function via microphone icon

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 /*DNT*/$(document).ready(function() {/*DNT*/
 
+  $('.fa-microphone').click(function() {
+
+    var newMsgText = $('#type-input').val();
+    console.log(newMsgText);
+
+    var newMsg = $('.template .msg').clone();
+
+    newMsg.text(newMsgText);
+
+    $('.msgs-container').append(newMsg);
+  })
+
 })/*DNT*/
