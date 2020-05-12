b9c1c4aa22399ab07304bd4824acefc20912f740
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-12 19:23:07 +0200

    working search filter

--- main.js
+++ main.js
@@ -1,19 +1,49 @@
 /*DNT*/$(document).ready(function() {/*DNT*/
 
-  $('.fa-microphone').click(function() {
-    sendMessage();
-  })
+$('.fa-microphone').click(function() {
+  sendMessage();
+})
 
-  $(document).keypress(function(event){
-    if (event.which == '13') {
-    sendMessage();
+$(document).keypress(function(event){
+  if (event.which == '13') {
+  sendMessage();
+  }
+});
+
+/*launch control every time a key is released (otherwise it will launch without considering the last character inputed)*/
+$('.searchbar input').keyup(function() {
+
+  /*grab user input, convert to lowercase, put in a value for later use*/
+  var searchFor = $('.searchbar input').val().toLowerCase();
+
+  /*for each contact name*/
+  $('.chatlist-info-name').each(function() {
+
+    /*grab content, convert to lowercase, save in var for later*/
+    var contactName = $(this).text().toLowerCase();
+
+    /*if what I'm searching for is included anywhere in the contacts' names*/
+    if(contactName.includes(searchFor)) {
+      /*show them*/
+      $(this).closest('.chatlist-item').show();
+      /*otherwise, hide*/
+    } else {
+      $(this).closest('.chatlist-item').hide();
+
     }
-  });
+
+  })
+})
 
 })/*DNT*/
 
 /*FUNCTIONS*/
 
+
+/*search contacts*/
+
+
+/*send a message with content from input, get automated reply*/
 function sendMessage() {
   /*grab user text from input*/
   var newMsgText = $('#type-input').val();
@@ -37,6 +67,7 @@ function sendMessage() {
   /*input reset*/
   $('#type-input').val('');
 
+  /*delay 1s*/
   setTimeout(function(){
     /*copy template*/
     var newMsg = $('.template .msg').clone();
@@ -49,8 +80,6 @@ function sendMessage() {
 
     /*send template copy to html*/
     $('.msgs-container').append(newMsg);
-    
-    /*delay 1s*/
   }, 1000);
 }
 }
