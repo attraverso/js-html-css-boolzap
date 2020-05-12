3716e7f441c766b58429c3b1ae315e5555f62d2c
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-12 16:33:59 +0200

    get automated reply when sending message

--- main.js
+++ main.js
@@ -5,8 +5,7 @@
   })
 
   $(document).keypress(function(event){
-    var keycode = (event.keyCode ? event.keyCode : event.which);
-    if(keycode == '13'){
+    if (event.which == '13') {
     sendMessage();
     }
   });
@@ -16,12 +15,42 @@
 /*FUNCTIONS*/
 
 function sendMessage() {
+  /*grab user text from input*/
   var newMsgText = $('#type-input').val();
   console.log(newMsgText);
 
+  /*copy template*/
   var newMsg = $('.template .msg').clone();
 
+  /*add class for sent msg*/
+  newMsg.addClass('msg-own');
+
+  /* the input isn't empty*/
+  if (newMsg != '') {
+
+  /*send input to the template copy*/
   newMsg.text(newMsgText);
 
+  /*send template copy to html*/
   $('.msgs-container').append(newMsg);
-};
+
+  /*input reset*/
+  $('#type-input').val('');
+
+  setTimeout(function(){
+    /*copy template*/
+    var newMsg = $('.template .msg').clone();
+
+    /*add class for incoming msg*/
+    newMsg.addClass('msg-incoming');
+
+    /*fill incoming message content*/
+    newMsg.children('p').text('ok');
+
+    /*send template copy to html*/
+    $('.msgs-container').append(newMsg);
+    
+    /*delay 1s*/
+  }, 1000);
+}
+}
