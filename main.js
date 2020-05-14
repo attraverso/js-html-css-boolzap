212d49ff37c8d91a4bca6fabcded8ce1694be82d
Annachiara Traverso <annachiara.traverso@gmail.com>
2020-05-14 19:12:49 +0200

    unresolved issue when sending messages

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 /*DNT*/$(document).ready(function() {/*DNT*/
 
-/***CURRENT VERSION: YOU CAN'T SEND A MESSAGE UNTIL YOU'VE SELECTED A CONTACT***/
-
-/*when you click on a contact, you see their chat*/
-
 /*when you click on a contact*/
 $('.chatlist-item').click(function() {
+
+  /**HIDE WELCOME PAGE**/
+  /*hide welcome pane*/
+  $('#advice').hide();
+  /*show chat pane???*/
+  $('.chat-container').addClass('active');
+
+  /**SWITCH CHATS**/
   /*use data attribute to grab the chat item you're clicking on*/
   var chatItem = $(this).data('contact');
+  console.log(chatItem);
   /*remove class active from every item in the chatlist*/
   $('.chatlist-item').removeClass('active');
   /*add class active to the item that matches the data attribute*/
@@ -16,6 +21,8 @@ $('.chatlist-item').click(function() {
   $('.msgs-container').removeClass('active');
   /*add class active to the msgs container that matches the data attribute*/
   $('.msgs-container[data-contact="' + chatItem + '"]').addClass('active');
+
+  /**MATCH CHAT PROFILE TO SELECTED CONTACT**/
   /*grab contact name*/
   var contactName = $(this).find('.chatlist-info-name').text();
   /*put it in the profile box at the top of the right panel*/
@@ -23,19 +30,25 @@ $('.chatlist-item').click(function() {
   /*grab contact profile pic*/
   var contactImg = $(this).find('.chatlist-pic-box img').attr('src');
   /*put it in the profile box at the top of the right panel*/
-  $('.options-profile img').attr('src', contactImg);
+  $('.chat-options-container .options-profile img').attr('src', contactImg);
 
+  /**SEARCH**/
   /*launch control every time a key is released (otherwise it will launch without considering the last character inputed)*/
   $('.searchbar input').keyup(function() {
     search();
-  })
+  });
 
-  /*send message*/
+  /**SHOW/HIDE MESSAGE DROPDOWN MENU ON CLICK**/
+  $('.msgs-container').on('click', '.msg-toggle', function() {
+    $(this).siblings('.msg-dropdown').toggleClass('active');
+  });
 
+
+  /**SEND MESSAGE**/
   /*on click on mic icon*/
   $('.fa-microphone').click(function() {
     sendMessage(chatItem);
-  })
+  });
   /*every time I hit enter*/
   $('.typebox input').keypress(function(event){
     if (event.which == '13') {
@@ -43,10 +56,17 @@ $('.chatlist-item').click(function() {
     }
   });
 })
+
 /*FUNCTIONS*/
 
-/*search contacts*/
+/**GET TIME for message**/
+function getTime() {
+  var date = new Date();
+  var currentTime = date.getHours() + ':' + date.getMinutes();
+  return currentTime;
+}
 
+/*SEARCH CONTACTS*/
 function search() {
   /*if the string is not empty (prevents check on all chats when you delete the last remaining character from the searchbar)*/
   if ($('.searchbar input') != '') {
@@ -68,8 +88,7 @@ function search() {
   }
 }
 
-/*send a message with content from input, get automated reply*/
-
+/*SEND MESSAGE with content from input, get automated reply*/
 function sendMessage(contact) {
   /*grab user text from input*/
   var newMsgText = $('#type-input').val();
@@ -77,12 +96,16 @@ function sendMessage(contact) {
   var newMsg = $('.template .msg').clone();
   /*add class for sent msg*/
   newMsg.addClass('msg-own');
-  /* the input isn't empty*/
+  /*if the input isn't empty*/
   if (newMsg != '') {
     /*send input to the template copy*/
-    newMsg.text(newMsgText);
+    newMsg.children('.msg-content').text(newMsgText);
+    /*get current time*/
+    var currentTime = getTime();
+    console.log('CT variable: ' + currentTime);
+    newMsg.children('.msg-time').text(currentTime);
     /*send template copy to html in the msgs-container of the active contact*/
-    $('.msgs-container[data-contact="' + contact + '"]').append(newMsg);
+    $('.msgs-container.active').append(newMsg);
     /*input reset*/
     $('#type-input').val('');
     /*delay 1s*/
@@ -92,9 +115,13 @@ function sendMessage(contact) {
       /*add class for incoming msg*/
       newMsg.addClass('msg-incoming');
       /*fill incoming message content*/
-      newMsg.children('p').text('ok');
+      newMsg.children('.msg-content').text('ok');
+      /*get current time*/
+      var currentTime = getTime();
+      console.log('CT variable: ' + currentTime);
+      newMsg.children('.msg-time').text(currentTime);
       /*send template copy to html*/
-      $('.msgs-container').append(newMsg);
+      $('.msgs-container.active').append(newMsg);
     }, 1000);
   }
 }
