b50343efb0148e451ade41f22c8ebd2873a64bdf
attraverso <attraverso.dev@gmail.com>
2020-07-29 14:55:18 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,32 +1,40 @@
+May 8th, 2020
+
+### Assignment:
+
 A mockup of Whatsapp Web's interface (features limited to current course level).
 
-Required features:
-1) create HTML/CSS structure
-2) possibility to add messages from user input
-3) automated answer after 1 second from user message
-4) working search that filters contacts matching search
-5) clicking on a contact on the left panel shows the correponging chat on the right
-6) ability to delete messages from a dropdown menu
+**Required features:**
+1. create HTML/CSS structure
+2. possibility to 'send' messages from user input and have them visually appear in-chat
+3. automated answer after 1 second from user message
+4. contact search that filters existing contacts
+5. clicking on a contact on the left panel shows the correponging chat on the right
+6. ability to delete single messages from a dropdown menu appearing on hover on the message itself
 
-Bonus features:
-x) random automated answers
+**Bonus features:**
+- layout features:
+  - a welcome page is displayed instead of a random chat
+  - the mic icon to the right of the message input switches to send icon when there's text in the input field
+- time-related features:
+  - every new message gets an updated timestamp that always shows up in hh:mm format
+  - 'last seen at' gets updated whenever entering a new chat to match the timestamp of its last messsage
+  - 'last seen at' gets updated at every new message in the corresponding chat
+  - the timestamp on the contact box gets updated at every new message in the corresponding chat
+- input-related features:
+  - empty messages cannot be sent
+  - empty searches are not considered
+  - searches are case-insensitive and start every time a key is pressed
+  - the search input can be cleared by means of a single, x-shaped button that only appears when there's text in the search input
+  - messages can be sent by pressing the enter key
+- the last message in a chat also gets displayed in the corresponding contact box preview
+- automated answers are picked at random from a predetermined set
 
-x) welcome page displayed before chats
 
-x) time-related features:
-  y) every new message gets an updated timestamp that always shows up in hh:mm format
-  y) 'last seen at' gets updated whenever entering a new chat to match the timestamp of its last messsage
-  y) 'last seen at' gets updated at every new message in the corresponding chat
-  y) the timestamp on the contact box gets updated at every new message in the corresponding chat
+### Goals:
 
-x) input-related features:
-  y) empty messages cannot be sent
-  y) empty searches are not considered
-  y) searches are case-insensitive
-  y) the search input can be cleared by means of a single button
-  y) when writing a message, the mic icon switches to send icon
-  y) messages can be sent by pressing the enter key
+A complex project to bring together the current level of HTML, CSS and JS knowledge.
 
-x) chat automatically scrolls to bottom when a new message is sent/received
-
-x) the last message in a chat is also shown in the corresponding contact box preview
+### Additional technologies:
+- jQuery
+- Handlebars.js
---
+++ take2.js
@@ -0,0 +1,233 @@
+/*DNT*/$(document).ready(function() {/*DNT*/
+
+/*when you click on a contact*/
+$('.chatlist-item').click(function() {
+
+  /**HIDE WELCOME PAGE**/
+  /*hide welcome pane*/
+  $('#advice').hide();
+  /*show chat pane???*/
+  $('.chat-container').addClass('active');
+
+  /**SWITCH CHATS**/
+  /*use data attribute to grab the chat item you're clicking on*/
+  chatItem = $(this).data('contact');
+  /*remove class active from every item in the chatlist*/
+  $('.chatlist-item').removeClass('active');
+  /*add class active to the item that matches the data attribute*/
+  $('.chatlist-item[data-contact="' + chatItem + '"]').addClass('active');
+  /*remove class active from every msgs container in the right panel*/
+  $('.msgs-container').removeClass('active');
+  /*add class active to the msgs container that matches the data attribute*/
+  $('.msgs-container[data-contact="' + chatItem + '"]').addClass('active');
+  /*grab timestamp of the last incoming message*/
+  var initialLastSeen = $('.msgs-container[data-contact="' + chatItem + '"]').children().last('.msg-incoming').children('.msg-time').text();
+  /*put it after 'last seen at' in chat-options*/
+  $('#options-profile-time').text(initialLastSeen);
+
+  /**MATCH CHAT PROFILE TO SELECTED CONTACT**/
+  /*grab contact name*/
+  var contactName = $(this).find('.chatlist-info-name').text();
+  /*put it in the profile box at the top of the right panel*/
+  $('.options-profile-info-name').text(contactName);
+  /*grab contact profile pic*/
+  var contactImg = $(this).find('.chatlist-pic-box img').attr('src');
+  /*put it in the profile box at the top of the right panel*/
+  $('.chat-options-container .options-profile img').attr('src', contactImg);
+})
+
+/**SEND MESSAGE**/
+/*on click on mic icon*/
+$('.fa-microphone').click(function() {
+  sendMessage(chatItem);
+  /*copy message in contacts preview*/
+  contactsLastMessage();
+});
+/*every time I hit enter*/
+$('.typebox input').keypress(function(event){
+  if (event.which == '13') {
+  sendMessage(chatItem);
+  /*copy message in contacts preview*/
+  contactsLastMessage();
+  }
+});
+
+/**CHANGE MIC ICON TO SEND WHEN WRITING MESSAGE**/
+/*when typebox input has focus*/
+$('.typebox input').focus(function() {
+  /*change icon*/
+  $('.typebox-container i:last-child').removeClass('fas fa-microphone');
+  $('.typebox-container i:last-child').addClass('fab fa-telegram-plane');
+})
+/*when typebox input loses focus*/
+$('.typebox input').blur(function() {
+  /*change icon*/
+  $('.typebox-container i:last-child').removeClass('fab fa-telegram-plane');
+  $('.typebox-container i:last-child').addClass('fas fa-microphone');
+})
+
+
+/**SEARCH**/
+/*launch control every time a key is released (otherwise it will launch without considering the last character inputed)*/
+$('.searchbar input').keyup(function() {
+  search();
+});
+
+/**SEARCH CROSS EMPTIES INPUT**/
+/*when click on cross, empty input*/
+// $('.chat-search-container').on('click', '.searchbar .fa-times.active', function() {
+//   $('.searchbar input').val('');
+//   $('.chatlist-item').show();
+// });
+
+// $('.searchbar .fa-times.active').click(function() {
+//   // $('.searchbar input').val('');
+//   alert('hello');
+// })
+
+/**DROPDOWN, MESSAGE REMOVAL**/
+/*show on click*/
+$('.msgs-container').on('click', '.msg-toggle', function() {
+  $(this).siblings('.msg-dropdown').toggleClass('active');
+  /*hide on mouseleave*/
+}).on('mouseleave', '.msg-dropdown', function() {
+  $('.msg-dropdown').removeClass('active');
+  /*delete message*/
+}).on('click', '.msg-dd-delete', function() {
+  /*completely remove message*/
+  // $(this).closest('.msg').remove();
+  /*replace message with reminder*/
+  $(this).parents().siblings('.msg-time').remove();
+  $(this).parents().siblings('.msg-content').html('<i class="fas fa-ban"></i><em> You deleted this message</em>');
+});
+
+
+
+/* * FUNCTIONS - alphabetical order * */
+
+/*SEND AUTOMATED REPLY after user message*/
+function automatedReply() {
+  /*copy template*/
+  var newMsg = $('.template .msg').clone();
+  /*add class for incoming msg*/
+  newMsg.addClass('msg-incoming');
+  /*fill incoming message content with random message*/
+  autoText = randomMsg();
+  newMsg.children('.msg-content').text(autoText);
+  /*get current time*/
+  var currentTime = getTime();
+  /*primt timestamp on new message*/
+  newMsg.children('.msg-time').text(currentTime);
+  /*update 'last seen at' in chat-options*/
+  $('#options-profile-time').text(currentTime);
+  /*send template copy to html*/
+  $('.msgs-container.active').append(newMsg);
+  /*scroll down 500px*/
+  $('.msgs-area').animate({
+      scrollTop: $('.msg:last-child').offset().top
+    }, 500);
+  /*copy message in contacts preview*/
+  contactsLastMessage();
+}
+
+/*GRAB LAST MESSAGE FROM CHAT to update contact preview*/
+function contactsLastMessage() {
+  /*grab last message's content*/
+  var msgContent = $('.msgs-container.active .msg:last-child .msg-content').text();
+  /*send to contact preview*/
+  $('.chatlist-item.active .chatlist-info-preview').text(msgContent);
+  /*grab last message's timestamp*/
+  var msgTime = $('.msgs-container.active .msg:last-child .msg-time').text();
+  /*send to contact preview*/
+  $('.chatlist-item.active .chatlist-timestamp').text(msgTime);
+
+}
+
+/**GET TIME for message**/
+function getTime() {
+  /*get current time*/
+  var date = new Date();
+  /*extract hours and minutes*/
+  var currentHours = date.getHours();
+  var currentMinutes = date.getMinutes();
+  /*if < 10, add a '0' before it, then reassemble*/
+  if (currentHours < 10) {
+    currentHours = '0' + currentHours;
+  }
+  if (currentMinutes < 10) {
+    currentMinutes = '0' + currentMinutes;
+  }
+  currentTime = currentHours + ':' + currentMinutes;
+  return currentTime;
+}
+
+/*GET RANDOM NUMBER*/
+function getRandomNr(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+/*GET RANDOM MESSAGE FOR AUTOMATED REPLY*/
+function randomMsg() {
+  /*establish possible replies*/
+  var choices = ['if you say so', 'oh really?', 'cool, cool', 'okay, then', 'lol', 'sure', 'uh-huh', 'oh come on', 'unbelievable', 'smh'];
+  /*get random number*/
+  var randomNr = getRandomNr(0,9);
+  /*pick reply from array based on random number*/
+  var reply = choices.slice(randomNr, (randomNr + 1)).toString();
+  return reply;
+}
+
+/*SEARCH CONTACTS*/
+function search() {
+  /*grab user input, convert to lowercase, put in a value for later use*/
+  var searchFor = $('.searchbar input').val().trim().toLowerCase();
+  /*if the string is not empty (prevents check on all chats when you delete the last remaining character from the searchbar)*/
+  if (searchFor != '') {
+    $('.searchbar .fa-times').addClass('active');
+    /*for each contact name*/
+    $('.chatlist-info-name').each(function() {
+      /*grab content, convert to lowercase, save in var for later*/
+      var contactName = $(this).text().toLowerCase();
+      /*if what I'm searching for is included anywhere in the contacts' names*/
+      if(contactName.includes(searchFor)) {
+        /*show them*/
+        $(this).closest('.chatlist-item').show();
+        /*otherwise, hide*/
+      } else {
+        $(this).closest('.chatlist-item').hide();
+      }
+    })
+  } else {
+    $('.searchbar .fa-times').removeClass('active');
+  }
+}
+
+/*SEND MESSAGE with content from input, get automated reply from other function*/
+function sendMessage(contact) {
+  /*grab user text from input*/
+  var newMsgText = $('#type-input').val();
+  /*copy template*/
+  var newMsg = $('.template .msg').clone();
+  /*add class for sent msg*/
+  newMsg.addClass('msg-own');
+  /*if the input isn't empty / only made of spaces*/
+  if ($('.typebox input').val().trim() != '') {
+    /*send input to the template copy*/
+    newMsg.children('.msg-content').text(newMsgText);
+    /*get current time*/
+    var currentTime = getTime();
+    newMsg.children('.msg-time').text(currentTime);
+    /*send template copy to html in the msgs-container of the active contact*/
+    $('.msgs-container.active').append(newMsg);
+    /*input reset*/
+    $('#type-input').val('');
+    /*scroll down 500px*/
+    $('.msgs-area').animate({
+        scrollTop: $('.msg:last-child').offset().top
+      }, 500);
+    /*delay 1s*/
+    setTimeout(automatedReply, 1000);
+  }
+}
+
+})/*DNT - closing document.ready*/
